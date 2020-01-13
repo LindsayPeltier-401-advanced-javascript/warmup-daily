56734ef4d3c6a865bd214a4380ceef780efb9bd6
LindsayPeltier <lindsaypeltier@gmail.com>
2020-01-13 11:03:21 -0800

    array and forloop

---
+++ 2020-01-13/2020-01-13.js
@@ -0,0 +1,25 @@
+'use strict';
+
+//Create an array of numbers, 1 - 10
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(array);
+
+//Write a function, called forLoop that takes an array as a parameter, runs the array through a for(...) loop and does a console.log() of each element.
+
+function forLoop(array) {
+  for (let i = 0; i <= array.length - 1; i++) {
+    console.log(array[i]);
+  }
+}
+
+forLoop(array);
+
+//Write a function, called whileLoop that takes an array as a parameter, runs the array through a while (...) loop and does a console.log() of each element.
+
+//Implement.map()
+
+//Implement .filter()
+
+//Implement .reduce()
