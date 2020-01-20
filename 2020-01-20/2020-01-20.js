d933561edf730843e4689819049820a646f84f55
LindsayPeltier <lindsaypeltier@gmail.com>
2020-01-20 14:11:23 -0800

    map

--- 2020-01-20/2020-01-20.js
+++ 2020-01-20/2020-01-20.js
@@ -25,3 +25,16 @@ function whileLoop (array) {
 }
 
 whileLoop(array);
+
+//implement .map()
+
+function map(array, callback) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(callback(array[i]));
+  }
+  return newArray;
+}
+
+map(array, x => (x += 1));
+
