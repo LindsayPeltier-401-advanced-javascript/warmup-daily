40f020a1b3fbc73df016e5cb70581adb66454982
LindsayPeltier <lindsaypeltier@gmail.com>
2020-01-20 14:07:56 -0800

    forLoop and whileLoop

---
+++ 2020-01-20/2020-01-20.js
@@ -0,0 +1,27 @@
+'use strict';
+
+//Create an Array of numbers, 1 - 10
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//Write a forLoop that takes in an array as a parameter, runs the array through the forLoop and console.logs each element
+
+function forLoop(array) {
+  for (let i = 0; i<= array.length -1; i++) {
+    console.log (array[i]);
+  }
+}
+
+forLoop(array);
+
+//Write a whileLoop that takes an array as a parameter, runs the array through a whileLoop, and console.logs each element
+
+function whileLoop (array) {
+  let i = 0;
+  while (i < array.length) {
+    console.log(array[i]);
+    i++;
+  }
+}
+
+whileLoop(array);
