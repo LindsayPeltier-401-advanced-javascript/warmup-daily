29253dda171b4cce07ca46087dd73b04ca03100c
LindsayPeltier <lindsaypeltier@gmail.com>
2020-01-22 13:54:11 -0800

    reduce

--- 2020-01-22/2020-01-22.js
+++ 2020-01-22/2020-01-22.js
@@ -48,3 +48,17 @@ function filter(array, callback) {
 filter(array, n => n === 3);
 
 // Reduce
+function reduce(array, callback) {
+  let newValue = 0;
+  for (let i = 0; i < array.length; i++) {
+    newValue = callback(newValue, array[i]);
+  }
+  return newValue;
+}
+var reducer = (accumulator, current) => {
+  return (accumulator += current);
+};
+console.log(reduce(array, reducer));
+
+/* Objects --------------------------------------------*/
+
