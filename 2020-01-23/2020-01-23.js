6b2ff20c48427d7ff98b292a8133b415ae16cee4
LindsayPeltier <lindsaypeltier@gmail.com>
2020-01-23 15:41:33 -0800

    reduce and filter

--- 2020-01-23/2020-01-23.js
+++ 2020-01-23/2020-01-23.js
@@ -32,3 +32,28 @@ function map(array, callback) {
 }
 map(array, x => (x += 1));
 
+// Filter
+function filter(array, callback) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
+filter(array, n => n === 3);
+
+// Reduce 
+function reduce(array, callback) {
+  let newValue = 0;
+  for (let i = 0; i < array.length; i++) {
+    newValue = callback(newValue, array[i]);
+  }
+  return newValue;
+}
+var reducer = (accumulator, current) => {
+  return (accumulator += current);
+};
+console.log(reduce(array, reducer));
+
