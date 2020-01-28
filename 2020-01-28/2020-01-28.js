2c7dd8591d5f75ad6d6a87bd1150200c424803b8
LindsayPeltier <lindsaypeltier@gmail.com>
2020-01-28 10:08:17 -0800

    forLoop

--- 2020-01-28/2020-01-28.js
+++ 2020-01-28/2020-01-28.js
@@ -1,4 +1,13 @@
 'use strict';
 
 // Array
-let array = [1,2,3,4,5,6,7,8,9,10];
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//forLoop
+function forLoop(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+}
+forLopp(array);
+
