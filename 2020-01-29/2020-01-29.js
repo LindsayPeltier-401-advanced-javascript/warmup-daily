3efadeb9a6e635acc5a3871598ab2af7a453084e
LindsayPeltier <lindsaypeltier@gmail.com>
2020-01-29 15:26:48 -0800

    loop and while

--- 2020-01-29/2020-01-29.js
+++ 2020-01-29/2020-01-29.js
@@ -2,3 +2,22 @@
 
 // Array
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//forLoop 
+function forLoop(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+}
+forLopp(array);
+
+// whileLoop
+function whileLoop(array) {
+  let i = 0;
+  while (i < array.length) {
+    console.log(array[i]);
+    i++;
+  }
+}
+whileLoop(array);
+
