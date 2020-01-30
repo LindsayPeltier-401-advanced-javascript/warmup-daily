f3ea462e5bb6813989cdccf8637f3341d5237b45
LindsayPeltier <lindsaypeltier@gmail.com>
2020-01-30 11:58:59 -0800

    array, forLoop, whileLoop

---
+++ 2020-01-30/2020-01-30.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Array
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//forLoop
+function forLoop(array) {
+  for (let i = 0; i <= array.length; i++) {
+    console.log(arra[i]);
+  }
+}
+forLoop(array);
+
+//whileLoop
+function whileLoop(array) {
+  let i = 0;
+  while (i < array.length) {
+    console.log(array[i]);
+    i++;
+  }
+}
+whileLoop(array);
+
