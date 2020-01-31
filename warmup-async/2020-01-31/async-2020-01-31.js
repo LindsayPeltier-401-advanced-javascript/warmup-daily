ea3c0bf899b1981c521c1b566f45100acf00bd47
LindsayPeltier <lindsaypeltier@gmail.com>
2020-01-31 14:09:28 -0800

    promises

---
+++ warmup-async/2020-01-31/async-2020-01-31.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Promises
+
+const superagent = require('superagent');
+const fetchPeopleWithPromises = () => {
+
+  return superagent.get('https://swapi.co/api/people')
+    .then(response => {
+      return response.body.result.map(person => {
+        return superagent.get(person.url)
+      })
+    })
+    .then(peopleRequest => {
+      return Promise.all(peopleRequests)
+        .then(people => {
+          let names = [];
+          for (let data of people) {
+            names.push(data.body.name);
+          }
+          return names;
+        })
+    })
+};
+fetchPeopleWithPromises()
+  .then(people => console.log({ people }));
+
+
+// ASYNC
+
