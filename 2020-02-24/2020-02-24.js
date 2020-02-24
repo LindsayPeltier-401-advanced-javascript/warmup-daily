6026dddfd900a79f0b3a7d1ab404beab9ddda2bc
LindsayPeltier <lindsaypeltier@gmail.com>
2020-02-24 13:57:44 -0800

    map, filter, reduce

--- 2020-02-24/2020-02-24.js
+++ 2020-02-24/2020-02-24.js
@@ -11,3 +11,47 @@ function forLoop(array) {
 }
 forLoop(array);
 
+//whileLoop
+function whileLoop(array) {
+  let i = 0;
+  while (i < array.length) {
+    console.log(array[i]);
+    i++;
+  }
+}
+whileLoop(array);
+
+//map
+function map(array, callback) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(callback(array[i]));
+  }
+  return newArray;
+}
+map(array, x => (x += 4));
+
+//filter
+function filter(array, callback) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
+filter(array, n => n === 1);
+
+//reduce
+function reduce(array, callback) {
+  let newValue = 0;
+  for (let i = 0; i < array.length; i++) {
+    newValue = callback(newValue, array[i]);
+  }
+  return newValue;
+}
+var reducer = (accumulator, current) => {
+  return (accumlator += current);
+};
+console.log(reduce(array, reducer));
