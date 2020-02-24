2841d85ff8f5b5ece6f193e14d21511f3b262892
LindsayPeltier <lindsaypeltier@gmail.com>
2020-02-24 13:52:42 -0800

    for and while loop

---
+++ 2020-02-24/2020-02-24.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//Array
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//forLoop
+function forLoop(array) {
+  for (var i = 0; i <= array.length; i++) {
+    console.log(array[i]);
+  }
+}
+forLoop(array);
+
